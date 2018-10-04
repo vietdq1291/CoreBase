afde8e54e5c291ff3a070f2f48a31f011dd91dd1
vietdq1291 <38650722+vietdq1291@users.noreply.github.com>
2018-10-04 16:52:30 +0700

    Check 2

---
+++ DataAccess/Infrastructure/IConnectionFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Data;
+
+namespace DataAccess.Infrastructure
+{
+    public interface IConnectionFactory : IDisposable
+    {
+        IDbConnection GetConnection { get; }
+    }
+}
